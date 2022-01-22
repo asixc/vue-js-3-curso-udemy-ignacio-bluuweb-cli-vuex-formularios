9808e24eb7af461fa56d83d949902c9a8a552722
Jotxee <jotxee@jotxee.dev>
2022-01-22 17:43:53 +0100

    add form for use v-model

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="home">
-    inicio
-  </div>
+  <form>
+    <input type="text"
+      class="form-control my-2"
+      placeholder="Ingrese su nombre"
+      v-model="tarea.nombre"
+    >
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox" 
+        name="" 
+        id="check-1"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="javascript"
+      >
+      <label for="check-1" class="form-check-label">JavaScript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox" 
+        name="" 
+        id="check-2"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="nodejs"
+      >
+      <label for="check-2" class="form-check-label">NodeJs</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox" 
+        name="" 
+        id="check-3"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="php"
+      >
+      <label for="check-3" class="form-check-label">PHP</label>
+    </div>
+
+    <p>nombre: {{tarea}} </p>
+  </form>
 </template>
 
 <script>
@@ -12,6 +51,14 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  data(){
+    return{
+      tarea:{
+        nombre: '',
+        categorias: []
+      }
+    }
   }
 }
 </script>
