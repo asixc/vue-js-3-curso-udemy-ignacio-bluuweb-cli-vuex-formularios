5ed02f3c18978c6de8fa623da0493e53773c5a8d
Jotxee <jotxee@jotxee.dev>
2022-01-23 18:55:20 +0100

    add new inputs to forms

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <form>
+  <form ref="formulario" @submit.prevent="procesarFormulario">
     <input type="text"
       class="form-control my-2"
       placeholder="Ingrese su nombre"
-      v-model="tarea.nombre"
+      v-model.trim="tarea.nombre"
     >
     <div class="form-check form-check-inline">
       <input 
@@ -39,6 +39,26 @@
       <label for="check-3" class="form-check-label">PHP</label>
     </div>
 
+
+  <div class="custom-control custom-radio custom-control-inline">
+    <input type="radio" id="customRadioInline1" name="customRadioInline" class="custom-control-input" value="urgente" v-model="tarea.urgente">
+    <label class="custom-control-label" for="customRadioInline1">Urgente</label>
+  </div>
+  <div class="custom-control custom-radio custom-control-inline">
+    <input type="radio" id="customRadioInline2" name="customRadioInline" class="custom-control-input" value="no-urgente" v-model="tarea.urgente">
+    <label class="custom-control-label" for="customRadioInline2">No urgente</label>
+  </div>
+  <div class="mt-2">
+    <input type="number" class="form-control" v-model.number="tarea.numero">
+  </div>
+
+  <button 
+    class="btn btn-dark mt-5 btn-block" 
+    type="submit"
+    :disabled="bloquear"
+    >
+    Enviar
+  </button>
     <p>nombre: {{tarea}} </p>
   </form>
 </template>
@@ -56,9 +76,35 @@ export default {
     return{
       tarea:{
         nombre: '',
-        categorias: []
+        categorias: [],
+        urgente: '',
+        numero: 0
       }
     }
+  },
+  methods:{
+    procesarFormulario() {
+      console.log(this.tarea)
+      if (this.tarea.nombre.trim() === "") {
+        console.log ('Campo nombre vacio')
+        return
+      }
+      console.log('enviando formulario...')
+      this.tarea = {
+        nombre: '',
+        categorias: [],
+        urgente: '',
+        numero: 0
+      }
+      // this.$refs.formulario.reset(); limpia el formulario pero no limpia el this.tarea
+      
+    }
+  },
+  computed: {
+    bloquear() {
+      return this.tarea.nombre.trim() === "" ? true : false
+    }
   }
+
 }
 </script>
